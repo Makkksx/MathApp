ba131d23d17c650dc8128def5864d688b45e893e
Вологдин Максим <m.vologdin@mail.ru>
2021-10-13 00:45:41 +0300

    Delete frontend directory

--- frontend/src/components/Admin.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UsersTable from "./AdminBlocks/UsersTable";
-import {Container} from "react-bootstrap";
-
-export const Admin = () => (
-    <Container>
-        <UsersTable/>
-    </Container>
-)
--- frontend/src/config/AuthMethods.js
+++
@@ -1,5 +0,0 @@
-import {FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider} from "firebase/auth";
-
-export const googleProvider = new GoogleAuthProvider();
-export const facebookProvider = new FacebookAuthProvider();
-export const githubProvider = new GithubAuthProvider();
